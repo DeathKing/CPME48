79f6aef33bac9711656487a78fe979cfc7214dc9
James Deng <deathking0622@gmail.com>
2014-07-19 11:06:05 +0800

    add a dasm sample.

---
+++ DASM48/sample/cpme48.asm
@@ -0,0 +1,26 @@
+; Example for cpme48.asm
+; Author: DeathKing<dk@hit.edu.cn>
+; Date:   7/2014
+
+.IR48
+.MODEL SIMPLE
+
+.DATA
+
+.CODE
+start:
+    IN      R0,0H
+    IN      R1,0H
+    ADD     R0,R1
+    STA     R0,0H
+    IN      R0,0H
+    LDA     R1,0H
+    SUB     R0,R1
+    JZ      R0,goback
+    OUT     R1,0
+goback:
+    JMP     start
+
+PUBLIC start
+
+END
